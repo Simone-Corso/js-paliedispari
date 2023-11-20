4ee53ff5de8eeef24149a7e17271d456a3a9eb29
Simone-Corso <scorso26@gmail.com>
2023-11-20 16:13:53 +0100

    ho creato una variabile con prompt dove chiedo qualsiasi parola all'utente

--- Palidroma/script.js
+++ Palidroma/script.js
@@ -1,3 +1,8 @@
 //Chiedere all’utente di inserire una parola
 //Creare una funzione per capire se la parola inserita è palindroma
 
+//Dobbiamo chiedere all'utente di inserire una parola qualsiasi
+
+    const chiediParola = prompt('Inserire qualsiasi parola');
+    console.log(chiediParola);
+//Nella console funziona nel chiedere qualsiasi parola.
